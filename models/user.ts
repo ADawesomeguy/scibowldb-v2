b1d00417806bb8cf425e56d0df3f8c82fae58abe
Abheek Dhawan <abheekd@protonmail.com>
2022-12-16 23:37:58 -0600

    feat: add status and role for users

--- models/user.ts
+++ models/user.ts
@@ -1,12 +1,26 @@
 import mongoose from 'mongoose';
 
+enum Role {
+    ADMIN,
+    MODERATOR,
+    USER
+}
+
+enum Status {
+    ACTIVE,
+    SUSPENDED,
+    BANNED
+}
+
 // todo: allow different services for same user
 // todo: add profile info, maybe a diff type
 export const userSchema = new mongoose.Schema({
     _id: {
         type: String, // email/user from GitHub
         required: true
-    }
+    },
+    role: Role,
+    status: Status,
     // rounds will have a prop or two that will equal this id
     // e.g. creator/submitter
 });
